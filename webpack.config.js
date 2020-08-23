d4e9b281020eb3080854e522797242692fd7096a
SidneyNemzer <SidneyNemzer@users.noreply.github.com>
2020-08-22 18:32:33 -0700

    move static files to 'assets', format some files

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = (env, args) => {
   return {
-    mode: args.mode || 'development',
+    mode: args.mode || "development",
 
-    devtool: args.mode !== 'production' && 'cheap-eval-source-map',
+    devtool: args.mode !== "production" && "cheap-eval-source-map",
 
     context: path.resolve(__dirname),
     output: {
-      path: path.resolve(__dirname, './build')
+      path: path.resolve(__dirname, "./build"),
     },
 
     module: {
@@ -24,21 +24,21 @@ module.exports = (env, args) => {
             // assets smaller than specified size as data URLs to avoid requests.
             {
               test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-              loader: 'url-loader',
+              loader: "url-loader",
               options: {
                 limit: 10000,
-                name: 'media/[name].[ext]'
-              }
+                name: "media/[name].[ext]",
+              },
             },
             // Compile Elm files
             {
               test: /\.elm$/,
               exclude: [/elm-stuff/, /node_modules/],
-              loader: 'elm-webpack-loader',
+              loader: "elm-webpack-loader",
               options: {
-                debug: args.mode !== 'production',
-                optimize: args.mode === 'production'
-              }
+                debug: args.mode !== "production",
+                optimize: args.mode === "production",
+              },
             },
             // "css" loader resolves paths in CSS and adds assets as dependencies.
             // "style" loader turns CSS into JS modules that inject <style> tags.
@@ -46,10 +46,7 @@ module.exports = (env, args) => {
             // in development "style" loader enables hot editing of CSS.
             {
               test: /\.css/,
-              use: [
-                'style-loader',
-                'css-loader'
-              ]
+              use: ["style-loader", "css-loader"],
             },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
@@ -62,26 +59,26 @@ module.exports = (env, args) => {
               // Also exclude `html`, `json`, and 'ejs' extensions so they get
               // processed by Webpack's internal loaders.
               exclude: [/\.js$/, /\.html$/, /\.json$/, /\.ejs$/],
-              loader: require.resolve('file-loader'),
+              loader: require.resolve("file-loader"),
               options: {
-                name: 'media/[name].[ext]'
-              }
-            }
-          ]
-        }
+                name: "media/[name].[ext]",
+              },
+            },
+          ],
+        },
         // ** STOP ** Are you adding a new loader?
         // Make sure to add the new loader(s) before the "file" loader.
-      ]
+      ],
     },
 
     plugins: [
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
         inject: true,
-        filename: 'index.html',
-        template: path.resolve(__dirname, './src/template.html')
+        filename: "index.html",
+        template: path.resolve(__dirname, "./src/template.html"),
       }),
-      new CopyWebpackPlugin([ { from: 'public', to: './' } ])
-    ]
-  }
-}
+      new CopyWebpackPlugin([{ from: "assets", to: "./" }]),
+    ],
+  };
+};
